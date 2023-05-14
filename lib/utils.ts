524be4ceeeb922c642f208c7ce456e1c137b9737
riza1999 <49582374+riza1999@users.noreply.github.com>
2023-05-14 14:30:10 +0700

    new lib wait

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const wait = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
